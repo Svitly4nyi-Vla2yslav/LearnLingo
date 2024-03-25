d2905fbf3ba4c245e357a37707086ed9ae8bc4a6
Svitly4nyi-Vla2yslav <126347711+Svitly4nyi-Vla2yslav@users.noreply.github.com>
2024-03-25 01:16:39 +0100

    refactoring

--- src/firebase.ts
+++ src/firebase.ts
@@ -4,17 +4,6 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyAXOHSnwXDXyo08kuoU4yEaQGLMAoHlhNo",
-//   authDomain: 'learnlingo-7d407.firebaseapp.com',
-//   projectId: 'learnlingo-7d407',
-//   storageBucket: 'learnlingo-7d407.appspot.com',
-//   messagingSenderId: '179165568182',
-//   appId: '1:179165568182:web:1909949e86bc66d4f99a17',
-//   databaseURL:
-//     'https://learnlingo-7d407-default-rtdb.europe-west1.firebasedatabase.app/',
-// };
-
 export const firebaseConfig = {
   apiKey: "AIzaSyCzZoPkbBprm3eu5lNt47f1GKMUHoBzPlo",
   authDomain: "learnlingo1.firebaseapp.com",
